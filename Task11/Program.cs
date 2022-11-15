519df6d9bf7f3d191e5fd3d0cdc94c428ae5e49e
Виталий Борисов <goldstar_0990@mail.ru>
2022-11-15 15:47:40 +0100

    Add Task12

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -7,13 +7,13 @@
 
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из отрезка 100-999 --> {number}");
-int KillsecondDigit(int num); // int nun = number
 
+int KillsecondDigit(int num) // int nun = number
 {
     int firstDigit = number / 100;
-    int seconDigit = number % 10;
+    int thirdDigit = number % 10;
     int result = firstDigit * 10 + thirdDigit;
     return result;
 }
 Console.WriteLine("Удаляем вторую цифру числа");
-Console.WriteLine($"Htpekmnfn = {KillsecondDigit(number)}");
+Console.WriteLine($"Результат = {KillsecondDigit(number)}");
