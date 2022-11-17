7a8c6737d28b135629d450a8f27960f92b70f84d
Виталий Борисов <goldstar_0990@mail.ru>
2022-11-17 23:00:57 +0100

    Add Task 10 и 15

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка {number}");
+
+int ShowDigitsSecond(int num)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10 / 10;
+    // int result = secondDigit / 10;
+    return secondDigit;
+
+}
+Console.WriteLine("Показывает вторую цифру");
+Console.WriteLine($"Результат = {ShowDigitsSecond(number)}");
---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число и является этот день выходным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Days(number) ? "ДА" : "НЕТ");
+
+bool Days(int num)
+
+{
+
+    return (number == 6 || number == 7);
+}
+
