0f07722a7a59ea01f1213ccc987d4189a9b9c162
Виталий Борисов <goldstar_0990@mail.ru>
2022-12-01 22:57:51 +0100

    Add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.WriteLine("Введите числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int NumberPositiv(int[] arr)
+{
+    int numb = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) numb++;
+    }
+    return numb;
+}
+int numb = NumberPositiv(arr);
+Console.WriteLine($"Пользователь ввел цифр больше 0: {numb}");
