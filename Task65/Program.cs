ce2852627744b05b0b517c8f59501c75125b6880
Виталий Борисов <goldstar_0990@mail.ru>
2022-12-09 14:51:37 +0100

    Add Task 63, 65, 67, 69

---
+++ Task65/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите первое натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int direction = 0;
+if (numberM < numberN)
+    direction = 0;
+else
+    direction = 1;
+
+void NumbersNToM(int numb1, int numb2)
+{
+    if (direction == 1)
+    {
+        if (numb1 >= numb2)
+        {
+            Console.Write($"{numb1} ");
+            NumbersNToM(numb1 - 1, numb2);
+        }
+    }
+    else
+    {
+        if (numb1 <= numb2)
+        {
+            Console.Write($"{numb1} ");
+            NumbersMtoN(numb1 + 1, numb2);
+        }
+    }
+}
+
+void NumbersMtoN(int numb1, int numb2)
+{
+    if (numb1 <= numb2)
+    {
+        Console.Write($"{numb1} ");
+        NumbersMtoN(numb1 + 1, numb2);
+    }
+}
+
+NumbersNToM(numberM, numberN);
+
+// if (numberM > numberN)
+// NumbersNToM(numberM, numberN);
+// else
+// NumbersMtoN(numberM, numberN);
---
+++ Task69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNumber(int number, int degree)
+{
+    if (degree == 0) return 1;
+    return DegreeNumber(number, degree - 1) * number;
+}
+
+Console.WriteLine(DegreeNumber(numberA, numberB));
